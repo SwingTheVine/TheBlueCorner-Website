6e357e605cbc5906551c26d9a5efea4948d77c5b
SwingTheVine <swingthevine@gmail.com>
2024-03-27 23:10:13 -0400

    Added content to main page

---
+++ js/profilePicture.js
@@ -0,0 +1,16 @@
+const contentP = document.querySelectorAll('.content-p');
+const names = [
+  'Scout107',
+  'SwingTheVine',
+  'Nexiy'
+];
+
+names.forEach(name => {
+  const name_regex = new RegExp(name, 'g');
+
+  contentP.forEach(container => {
+    console.log(name);
+    const nameWithImage = container.innerHTML.replace(name_regex, '<img class="profile-icons" src="./assets/icons/' + name + '_Icon.webp" height="16"> ' + name);
+    container.innerHTML = nameWithImage;
+  })
+})
