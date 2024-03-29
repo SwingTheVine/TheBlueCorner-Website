2c4e724928c24ddb06f7ac30137429c22788bc3e
SwingTheVine <swingthevine@gmail.com>
2024-03-29 14:31:27 -0400

    Finished skeleton of Hall of Fame

--- js/profilePicture.js
+++ js/profilePicture.js
@@ -9,7 +9,7 @@ names.forEach(name => {
   const name_regex = new RegExp(name, 'g');
 
   contentP.forEach(container => {
-    const nameWithImage = container.innerHTML.replace(name_regex, '<img class="profile-icons" src="./assets/icons/' + name + '_Icon.webp" height="16"> ' + name);
+    const nameWithImage = container.innerHTML.replace(name_regex, '<img class="profile-icons" src="./assets/icons/pfp/' + name + '_Icon.webp" height="16"> ' + name);
     container.innerHTML = nameWithImage;
   })
 })
