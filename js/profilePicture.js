f974a4440a76e192cac2283eb28003b4059be39b
SwingTheVine <swingthevine@gmail.com>
2024-03-29 22:22:29 -0400

    Fixed pfp searching matching names in hyperlinks

--- js/profilePicture.js
+++ js/profilePicture.js
@@ -1,4 +1,7 @@
+// Creates an array of all content-p bodies of text.
 const contentP = document.querySelectorAll('.content-p');
+
+// Creates an array of all of the names to search for.
 const names = [
   '9kz7',
   'Bratishkinoff',
@@ -18,10 +21,17 @@ const names = [
   'Zugu'
 ];
 
+// For each name...
 names.forEach(name => {
-  const name_regex = new RegExp(name, 'gi');
 
+  // Create a new RegEx to search for that name. case-INsensitive
+  // If the name is inside another container (within <>) it is ignored.
+  // For example, <a href="name"> is ignored.
+  const name_regex = new RegExp('(?![^<]*>|[^<>]*<\/)(' + name + ')', 'gi');
+
+  // For every body of text...
   contentP.forEach(container => {
+    // Replace the name with the pfp + name
     const nameWithImage = container.innerHTML.replace(name_regex, '<img class="profile-icons" src="./assets/icons/pfp/' + name + '_Icon.webp" height="16"> ' + name);
     container.innerHTML = nameWithImage;
   })
