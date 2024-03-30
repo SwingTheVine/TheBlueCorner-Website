908e9b6e9ab76530f811e8dc662e00e5a972c2d8
SwingTheVine <swingthevine@gmail.com>
2024-03-29 21:55:51 -0400

    r/place 2022 First Corner translated

--- js/profilePicture.js
+++ js/profilePicture.js
@@ -1,18 +1,25 @@
 const contentP = document.querySelectorAll('.content-p');
 const names = [
   '9kz7',
+  'Bratishkinoff',
+  'ChippedJosh',
   'Chuck',
+  'Destiny',
   'j75',
   'Nexiy',
   'Scout107',
   'SwingTheVine',
+  'Tarik',
   'TheRageBot',
   'TheStruggleIsALie',
-  'Xenoglitch'
+  'Tumblurr',
+  'Xenoglitch',
+  'xQc',
+  'Zugu'
 ];
 
 names.forEach(name => {
-  const name_regex = new RegExp(name, 'g');
+  const name_regex = new RegExp(name, 'gi');
 
   contentP.forEach(container => {
     const nameWithImage = container.innerHTML.replace(name_regex, '<img class="profile-icons" src="./assets/icons/pfp/' + name + '_Icon.webp" height="16"> ' + name);
