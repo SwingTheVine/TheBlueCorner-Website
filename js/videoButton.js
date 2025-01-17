89ef9d697d8636c9d2e38a7c543be9dc37ff4237
SwingTheVine <swingthevine@gmail.com>
2025-01-16 19:33:31 -0500

    Improved video button aesthetics

--- js/videoButton.js
+++ js/videoButton.js
@@ -24,13 +24,37 @@ function triggerButton(button) {
   // If the video is paused...
   if (video.paused) {
     video.play(); // Play the video
-    button.textContent = "⏸"; // Override the icon
-    button.setAttribute('aria-label', "Pause the video using the 'space' or 'tab' keys."); // Update the ARIA label
+
+    // Kills all children
+    button.innerHTML = '';
+
+    // Creates a new child
+    var pauseIcon = document.createElement('img');
+    pauseIcon.className = 'video-button-image';
+    pauseIcon.src = './assets/icons/FontAwesome_Circle-Pause.svg';
+    pauseIcon.setAttribute('aria-label', ''); // Sets img as decorative
+    button.appendChild(pauseIcon);
+    
+    // Updates the ARIA values
+    button.setAttribute('aria-label', "Pause the video.");
+    button.setAttribute('aria-details', "You can pause the video using the 'space' or 'tab' keys.");
   } else {
     // ...the video is playing
 
     video.pause(); // Pause the video
-    button.textContent = "▶"; // Override the icon
-    button.setAttribute('aria-label', "Play the video using the 'space' or 'tab' keys."); // Update the ARIA label
+    
+    // Kills all children
+    button.innerHTML = '';
+
+    // Creates a new child
+    var playIcon = document.createElement('img');
+    playIcon.className = 'video-button-image';
+    playIcon.src = './assets/icons/FontAwesome_Circle-Play.svg';
+    playIcon.setAttribute('aria-label', ''); // Sets img as decorative
+    button.appendChild(playIcon);
+    
+    // Updates the ARIA values
+    button.setAttribute('aria-label', "Play the video.");
+    button.setAttribute('aria-details', "You can play the video using the 'space' or 'tab' keys.");
   }
 }
