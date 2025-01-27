736bcd3998faf77d2df2bbd39e0fdcb5e6c4b2ae
SwingTheVine <swingthevine@gmail.com>
2025-01-27 02:35:17 -0500

    Fixed profilePicture.js; Fixed some semantics

--- js/profilePicture.js
+++ js/profilePicture.js
@@ -1,6 +1,3 @@
-// Creates an array of all paragraphs of text.
-const paragraph = document.querySelectorAll('p');
-
 // Creates an array of all of the names to search for.
 const names = [
   '5opka',
@@ -53,15 +50,19 @@ const names = [
   'Zugu'
 ];
 
-// Create a new RegEx to search for every name. case-INsensitive
+// Creates an array of all elements of text we want to search.
+const targetContainers = document.querySelectorAll('p, li, h1, h2, h3, h4, h5, blockquote, small');
+
+// Create a RegEx to search for every name. case-INsensitive
 // If the name is inside another container (within <>) it is ignored.
 // For example, <p><a href="SwingTheVine"></p> is ignored (will not match).
 const name_regex = new RegExp('(?![^<]*>|[^<>]*<\/)(' + names.join('|') + ')', 'gi');
 
-// For every paragraph of text...
-paragraph.forEach(container => {
+// For every element container...
+targetContainers.forEach(container => {
+  
   // Replace the name with the pfp + name
   container.innerHTML = container.innerHTML.replace(name_regex, name => {
-    return '<span><img class="profile-icon" src="./assets/icons/pfp/' + name + '_Icon.webp" height="16" alt="">' + name + '</span>';
+    return '<span><img class="profile-icon" src="./assets/icons/pfp/' + name + '_Icon.webp" loading="lazy" alt="">' + name + '</span>';
   });
 });
